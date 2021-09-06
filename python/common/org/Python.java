3a93c974fa7768ebe4da872eb8b3557e1d5ee498
Maja Danielsson <danielsson.maja96@gmail.com>
2021-09-06 10:57:35 +0200

    First attempt at perm function

--- python/common/org/Python.java
+++ python/common/org/Python.java
@@ -1774,6 +1774,28 @@ public class Python {
     }
 
     @org.python.Method(
+    __doc__ = "Return the number of ways to choose k items from n items 
+            without repetition and with order.",
+    args = {"n", "k"}
+    )
+    public org.python.Object perm(org.python.Object n, org.python.Object k) {
+        if type(n) != int {
+            throw new TypeError("n must be an integer");
+        }
+        if (k == null) {
+            k = org.python.types.Int.getInt(n);
+        }
+        if (n < 0 || k < 0) {
+            throw new ValueError("Both arguments must be positive integers");
+        }
+
+        if (k <= n) {
+                return fact(n)/fact(n-k);
+            } else {
+                return 0;
+            }
+            
+    @org.python.Method(
             __doc__ = "type(object_or_name, bases, dict)" +
                     "type(object) -> the object's type\n" +
                     "type(name, bases, dict) -> a new type\n",
---
+++ tests/math/test_perm.py
@@ -0,0 +1,34 @@
+from ..utils import TranspileTestCase
+
+class PermTests(TranspileTestCase):
+
+    def test_valid_input(self):
+        self.assertCodeExecution("""
+            import math
+            print(perm(30,4))
+        """)
+
+    def test_wrong_type_input(self):
+         self.assertCodeExecution("""
+            import math
+            try:
+                print(perm('s',1))
+            except TypeError as e:
+                print(e)
+        """)
+
+    def test_null_input(self):
+        self.assertCodeExecution("""
+            import math
+            print(perm(3,None))
+        """)
+
+    def test_negative_input(self):
+        self.assertCodeExecution("""
+            import math 
+            try:
+                print(perm(-1, 5))
+            except ValueError as err:
+                print(err)
+            """)
+
