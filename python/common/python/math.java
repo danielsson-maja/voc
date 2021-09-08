451770162c2981c2154e97146f2e159a49ab9663
Kawthar <k.el@live.fr>
2021-09-08 08:45:37 +0200

    moving math to tests/stdlib

--- python/common/python/math.java
+++ python/common/python/math.java
@@ -16,18 +16,19 @@ package python;
 public class math extends org.python.types.Module {
 
     @org.python.Method(
-        name = "prod",
-        __doc__ = "Calculate the product of all the elements in the input iterable. " +
-                    "The default start value for the product is 1. " +
-                    "When the iterable is empty, return the start value. " +
-                    "This function is intended specifically for use with numeric values and may reject non-numeric types.",
-        args = {"iterable"},
-        default_args = {"start"}
+            name = "prod",
+            __doc__ = "Calculate the product of all the elements in the input iterable. " +
+                        "The default start value for the product is 1. " +
+                        "When the iterable is empty, return the start value. " +
+                        "This function is intended specifically for use with numeric values " +
+                        "and may reject non-numeric types.",
+            args = {"iterable"},
+            default_args = {"start"}
     )
     public static org.python.Object prod(org.python.Object iterable, org.python.Object start) {
         org.python.Object product;
         if (start != null) {
-            product = start;
+            product = start; 
         } else {
             product = org.python.types.Int.getInt(1);
         }
