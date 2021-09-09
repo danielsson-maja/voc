7e5b9c80b72e643442474a64329799bf174d6089
lovvvan <lovisa.hedman93@gmail.com>
2021-09-09 15:07:55 +0200

    added a transpiletestcase

--- stdlib_tests/test_factorial.py
+++ stdlib_tests/test_factorial.py
@@ -1,1 +1,11 @@
-print(factorial(3))
+#print(factorial(3))
+
+from tests.utils import TranspileTestCase
+
+class factorialTest(TranspileTestCase):
+    def test_positive_integers(self):
+        print("Test positive integers")
+        self.assertCodeExecution("""
+            import math
+            print(math.factorial(3))
+        """)
