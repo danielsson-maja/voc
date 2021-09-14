b449b7ee109cac7bd399cefa15d7216779688c3b
perpelea1 <35214115+perpelea1@users.noreply.github.com>
2021-09-14 16:58:15 +0200

    fixing floor

---
+++ tests/stdlib/math/test_floor.py
@@ -0,0 +1,41 @@
+from tests.utils import TranspileTestCase, BuiltinFunctionTestCase
+
+
+class FloorTests(TranspileTestCase):
+
+    def test_floor_string(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.floor("Test floor"))
+            """)
+
+    def test_floor_positive(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.floor(2))
+            """)
+
+    def test_floor_negative(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.floor(-2))
+            """)
+
+    def test_floor_double(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.floor(5.5))
+            """)
+
+    def test_floor_zero(self):
+        self.assertCodeExecution("""
+            import math
+            print(math.floor(0))
+            """)
+
+
+class BuiltinFloorFunctionTests(BuiltinFunctionTestCase, TranspileTestCase):
+    functions = ["floor"]
+
+    not_implemented = [
+    ]
