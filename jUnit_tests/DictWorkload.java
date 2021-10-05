73ecef114c580ec9bd27e14e03e841775d4c93a9
anhvu2501 <vu25ck28@gmail.com>
2021-10-05 13:32:01 +0200

    some update in workload

--- jUnit_tests/DictWorkload.java
+++ jUnit_tests/DictWorkload.java
@@ -2,8 +2,10 @@
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.python.Object;
 import org.python.java.data.Post;
 import org.python.types.Dict;
+import org.python.types.Int;
 import org.python.types.Str;
 
 import java.io.IOException;
@@ -40,25 +42,47 @@ public class DictWorkload {
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         DictWorkload dictWorkload = new DictWorkload();
         List<Post> posts = dictWorkload.readPost();
-        Map<org.python.Object, org.python.Object> dict_map = new HashMap<>();
+        Dict dict = new Dict();
         for (int i = 0; i < posts.size(); i++) {
-            dict_map.put(new Str(posts.get(i).getId()), new Str(posts.get(i).getTitle()));
+            dict.__setitem__(new Str(posts.get(i).getId()), new Str(posts.get(i).getTitle()));
         }
-//        System.out.println(posts.size());
-        Dict dict = new Dict(dict_map);
         Dict dict1 = (Dict) dict.copy();
-        System.out.println("dict" + dict.__repr__().toString());
-        System.out.println("dict1 " + dict1.__repr__().toString());
-        System.out.println("dict keys " + dict.keys().toString());
-        System.out.println("dict items " + dict.items().toString());
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 1500; i++) {
             dict1.popitem();
         }
-        System.out.println("dict1 after poping " + dict1.__repr__().toString());
+
+        //Create List of Dict
+        List<Map<org.python.Object, org.python.Object>> dicts = new ArrayList<>();
+        List<Object> objectList = posts.stream()
+            .map(post -> dicts.stream()
+                .map(dict2 -> dict2.put(new Str(post.getId()), new Str(post.getTitle())))
+                .collect(toList()))
+            .flatMap(Collection::stream)
+            .collect(toList());
+        List<Dict> dictList = objectList.stream()
+            .map(object -> {
+                Dict dict2 = new Dict();
+                dict2.__setitem__(object.__getitem__(Int.getInt(0)), new Str(""));
+                return dict2;
+            })
+            .collect(toList());
+
         Dict dict2 = (Dict) Dict.fromkeys(dict1, new Str("New book"));
-        System.out.println("dict 2 " + dict2.__repr__().toString());
+        dict2.update(dict, dict1);
+        for (int i = 0; i < 1000; i++) {
+            Object popitem = dict2.popitem();
+            Random random = new Random();
+            int newval = random.nextInt();
+            dict1.__setitem__(popitem.__getitem__(Int.getInt(0)), new Str("" + newval));
+        }
+        for (int i = 0; i < posts.size(); i++) {
+            dict2.__setitem__(new Str(posts.get(i).getId()), new Str("old book"));
+        }
         System.out.println("dict length " + dict.__len__().toString());
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("Runtime: " + endTime);
     }
 }
